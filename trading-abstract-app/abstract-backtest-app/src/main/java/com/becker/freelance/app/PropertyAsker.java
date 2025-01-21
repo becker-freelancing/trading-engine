3586287835aa8c506525693862926fbae8b99a2f
jasb <jason.becker@parcIT.de>
2025-01-21 14:36:32 +0100

    Abstract Trading App added

---
+++ trading-abstract-app/abstract-backtest-app/src/main/java/com/becker/freelance/app/PropertyAsker.java
@@ -0,0 +1,38 @@
+package com.becker.freelance.app;
+
+import com.becker.freelance.strategies.BaseStrategy;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+import java.util.Scanner;
+import java.util.function.Function;
+
+class PropertyAsker {
+
+    private static final Logger logger = LoggerFactory.getLogger(PropertyAsker.class);
+
+    public <T> T askProperty(List<T> selections, Function<T, String> toStringConverter, String name){
+        logger.info("\t\t===============================================================================");
+        logger.info("\t\t================== Wähle eine {} für den Backtest ==================", name);
+        logger.info("\t\t===============================================================================");
+
+        for (int i = 1; i <= selections.size(); i++){
+            T selection = selections.get(i - 1);
+            logger.info("\t\t{}.\t{}", i, toStringConverter.apply(selection));
+        }
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            try {
+                logger.info("\t\tGeben Sie die Nummer der gewünschten {} ein: ", name);
+                int choice = Integer.parseInt(scanner.nextLine());
+                if (choice >= 1 && choice <= selections.size()) {
+                    return selections.get(choice - 1);
+                } else {
+                    return selections.get(selections.size() - 1);
+                }
+            } catch (NumberFormatException ignored) {
+            }
+        }
+    }
+}
