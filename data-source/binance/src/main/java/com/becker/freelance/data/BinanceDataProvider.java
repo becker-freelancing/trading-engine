06f9b3f0e04f4200109926305f926e46604a982d
jasb <jason.becker@parcIT.de>
2025-03-15 07:44:22 +0100

    BugFix BinanceDataProvider

--- data-source/binance/src/main/java/com/becker/freelance/data/BinanceDataProvider.java
+++ data-source/binance/src/main/java/com/becker/freelance/data/BinanceDataProvider.java
@@ -73,10 +73,42 @@ public class BinanceDataProvider extends DataProvider {
         } catch (CsvException e) {
             throw new IOException(e);
         }
-        Map<LocalDateTime, TimeSeriesEntry> entries = mapWithSpread(rows, pair, from, to);
+        Map<LocalDateTime, TimeSeriesEntry> entries;
+
+        if (pair.equals(Pair.eurUsd1())) {
+            entries = mapEurUsd(rows, pair, from, to);
+        } else {
+            entries = mapWithSpread(rows, pair, from, to);
+        }
         TimeSeries timeSeries = map(pair, from, to, entries);
         logger.info("Finished reading TimeSeries {}", pair.technicalName());
         return timeSeries;
+    }
+
+    private Map<LocalDateTime, TimeSeriesEntry> mapEurUsd(List<String[]> rows, Pair pair, LocalDateTime from, LocalDateTime to) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        LocalDateTime beforeFrom = from.minusDays(3);
+        return rows.stream().skip(1).parallel().map(row -> {
+                    LocalDateTime time = LocalDateTime.parse(row[0], formatter);
+                    if (time.isBefore(beforeFrom) || time.isAfter(to)) {
+                        return null;
+                    }
+                    Decimal openBid = new Decimal(row[1]);
+                    Decimal openAsk = new Decimal(row[2]);
+                    Decimal highBid = new Decimal(row[3]);
+                    Decimal highAsk = new Decimal(row[4]);
+                    Decimal lowBid = new Decimal(row[5]);
+                    Decimal lowAsk = new Decimal(row[6]);
+                    Decimal closeBid = new Decimal(row[7]);
+                    Decimal closeAsk = new Decimal(row[8]);
+                    Decimal volume = Decimal.ZERO;
+                    Decimal trades = Decimal.ZERO;
+                    return new TimeSeriesEntry(
+                            time, openBid, openAsk, highBid, highAsk, lowBid, lowAsk,
+                            closeBid, closeAsk, volume, trades, pair
+                    );
+                }).filter(Objects::nonNull)
+                .collect(Collectors.toMap(TimeSeriesEntry::time, entry -> entry, (existing, replacement) -> existing));
     }
 
     private String getFilename(Pair pair) {
