fa3aa93259eb173af575af1b6aa63cfcbbe13974
jasb <jason.becker@parcIT.de>
2025-04-04 15:07:36 +0200

    Export Trade History to csv

--- broker-connectors/bybit-connector/src/main/java/com/becker/freelance/bybit/apps/RequestTradeHistory.java
+++ broker-connectors/bybit-connector/src/main/java/com/becker/freelance/bybit/apps/RequestTradeHistory.java
@@ -22,7 +22,7 @@ import java.util.stream.Stream;
 public class RequestTradeHistory {
 
     public static void main(String[] args) throws IOException {
-        LocalDateTime exportStart = LocalDateTime.parse("2025-04-01T00:00:00");
+        LocalDateTime exportStart = LocalDateTime.parse("2025-04-04T07:50:00");
         LocalDateTime exportEnd = LocalDateTime.parse("2025-04-05T00:00:00");
 
 
