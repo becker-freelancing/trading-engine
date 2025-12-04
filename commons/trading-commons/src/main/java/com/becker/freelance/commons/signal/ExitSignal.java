8f49026e3b8ed8b11f0ec31b4b7689162bb9a8c6
jason.becker <jason.becker@crownpeak.com>
2025-12-04 07:14:48 +0100

    Doc

--- commons/trading-commons/src/main/java/com/becker/freelance/commons/signal/ExitSignal.java
+++ commons/trading-commons/src/main/java/com/becker/freelance/commons/signal/ExitSignal.java
@@ -2,6 +2,42 @@ package com.becker.freelance.commons.signal;
 
 import com.becker.freelance.commons.position.Direction;
 
-public record ExitSignal(Direction directionToClose) {
+import java.util.Objects;
+
+public final class ExitSignal {
+
+
+    public static final ExitSignal CLOSE_ALL_BUY = new ExitSignal(Direction.BUY);
+
+    public static final ExitSignal CLOSE_ALL_SELL = new ExitSignal(Direction.SELL);
+    private final Direction directionToClose;
+
+    public ExitSignal(Direction directionToClose) {
+        this.directionToClose = directionToClose;
+    }
+
+    public Direction directionToClose() {
+        return directionToClose;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) return true;
+        if (obj == null || obj.getClass() != this.getClass()) return false;
+        var that = (ExitSignal) obj;
+        return Objects.equals(this.directionToClose, that.directionToClose);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(directionToClose);
+    }
+
+    @Override
+    public String toString() {
+        return "ExitSignal[" +
+                "directionToClose=" + directionToClose + ']';
+    }
+
 
 }
